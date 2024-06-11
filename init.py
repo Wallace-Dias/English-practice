895c8c739769e4ae94e8788e6982624601efd357
Wallace Medeiros Dias <wallacemedeiros.d@gmail.com>
2024-06-11 19:42:18 -0300

    ✨ Terminal - Clear
    Agora o terminal é limpo a cada iteração do laço for, isso facilita na visualização, também foi adicionado um arquivo md de padrões de commits que peguei do Iuri Silva
    iuricode, pretendo retirar futuramente e estou adicionando para facilitar meus commits e me ajudar a melhora-los no processo.

--- init.py
+++ init.py
@@ -1,6 +1,10 @@
 """ Quero criar um programa que me ajude a praticar meu inglês, vou tentar criar algo como um pergunta e resposta para testar """
-
+import os
 from random import choice
+from time import sleep
+
+def clear_screen():
+    os.system('cls' if os.name == 'nt' else 'clear')
 
 print("="*30)
 print("Pratique seu inglês!".center(30))
@@ -8,21 +12,17 @@ print("="*30)
 
 words = {'how': "como", 'are': 'são'}
 
-""" resp = 'sã'.lower()
-
-if resp in words.values():
-    print("Certo")
-else:
-    print("Errado") """
-
 print("Responda em Portuguese")
 for k, v in words.items():
+    clear_screen()
     print(f"{k}", end='')
     resp = str(input(": ")).lower()
     if resp in v:
         print("Você acertou!")
+        sleep(2)
     else:
         print("Incorreto!")
+        sleep(2)
 
 
 #print(words.keys())
